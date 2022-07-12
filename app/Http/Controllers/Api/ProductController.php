59ae6dd2d0339762980e2d6b98aa5b98def2e6cf
Marcel Stadelmann <m@marceli.to>
2022-07-12 16:51:38 +0200

    test

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -47,8 +47,6 @@ class ProductController extends Controller
     {
       $api_data = session('elbridge');
     }
-    dd($api_data);
-
     return response()->json(['products' => $products, 'filter_options' => $this->filter_options, 'api' => $api_data]);
   }
 
