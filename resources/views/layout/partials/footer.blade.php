4ca6fb7a19672d96c6c115015bb388175b2c62ac
Marcel Stadelmann <m@marceli.to>
2022-08-26 14:42:53 +0200

    add intercom

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -34,4 +34,13 @@
 <script src="{{ mix('assets/js/language.js') }}" type="text/javascript"></script>
 <script src="{{ mix('assets/js/form.js') }}"></script>
 <script src="{{ mix('assets/js/app.js') }}" defer></script>
+<script>
+  window.intercomSettings = {
+    app_id: "bqaelhed",
+    custom_launcher_selector: ".js-btn-livechat"
+  };
+</script>
+<script>
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/bqaelhed';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+</script>
 <!-- made with ❤ by marceli.to -->
